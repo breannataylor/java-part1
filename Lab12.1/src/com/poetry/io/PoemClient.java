25c1db886434d3d827e1a8518971e63cb27d0ef8
BreAnna Taylor <thebreannataylor@gmail.com>
2023-10-31 09:22:07 -0700

    Lab12.1 (Reading and Writing Files) complete

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -8,8 +8,7 @@
 
 package com.poetry.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 
 public class PoemClient {
 
@@ -17,8 +16,8 @@ public class PoemClient {
      * To run one method at a time, uncomment the call to the one you want to execute.
      */
     public static void main(String[] args) {
-        // readPoem();
-        // writePoem();
+         //readPoem();
+         writePoem();
     }
 
     /**
@@ -35,8 +34,12 @@ public class PoemClient {
      */
     private static void readPoem() {
         // TODO: initialize 'reader' variable and complete the try block
-        try (BufferedReader reader = null) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
+            String line;
+            while ( (line = reader.readLine()) != null){ //null means end of file (EOF)
+                System.out.println(line);
 
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -55,6 +58,14 @@ public class PoemClient {
      * Use a try-with-resources to initialize the stream and auto-close it.
      */
     private static void writePoem() {
-        // TODO
+       try (PrintWriter writer = new PrintWriter(new FileWriter("haiku.txt"))) {
+           writer.println("Morning chill in air");
+           writer.println("The world unburdened by growth");
+           writer.println("Winter is coming");
+       }
+       catch (IOException e) {
+           e.printStackTrace();
+
+       }
     }
 }
